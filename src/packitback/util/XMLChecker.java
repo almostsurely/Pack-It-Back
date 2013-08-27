6d8196c03558aed0c7346986474a8c3fcd2f0204
almostsurely <monsieurdozier@gmail.com>
2013-08-27 17:41:38 -0500

    Finished basic XML checks.

--- src/packitback/util/XMLChecker.java
+++ src/packitback/util/XMLChecker.java
@@ -1,6 +1,5 @@
 package packitback.util;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.jdom2.Document;
@@ -86,6 +85,66 @@ public class XMLChecker {
 				if(!checkHas(set, "Build")){
 					valid = false;
 					builder.append(setName + " in " + gameName + " doesn't have any <Build> tags.\n\n");
+				} else {
+					
+					List<Element> builds = set.getChildren("Build");
+					
+					//For each Build in Set
+					for (Element build : builds){
+						
+						//Check that each <Build> has a <Level>
+						if(!checkHas(build, "Level")){
+							valid = false;
+							builder.append("A <Build> in " + setName + " in " + gameName + " doesn't have a <Level>.\n\n");
+						} else {
+							
+							List<Element> levels = build.getChildren("Level");
+							
+							//For each Level in Build
+							for (Element level : levels){
+								if(!checkHasName(level)){
+									valid = false;
+									builder.append("A <Level> in a <Build> in " + setName + " in " + gameName + " doesn't have a <Name>\n\n");
+								}
+								
+								if(!checkHas(level, "Count")){
+									valid = false;
+									builder.append("A <Level> in a <Build> in " + setName + " in " + gameName + " doesn't have a <Count>\n\n");
+								}
+							}
+						}
+						
+						//Check that each <Build> has a <Weight>
+						if(!checkHas(build, "Weight")){
+							valid = false;
+							builder.append("A <Build> in " + setName + " in " + gameName + " doesn't have a <Weight>.\n\n");
+						}
+					}
+				}
+				
+
+				
+				//Check that each <Set> has a <Card>
+				if(!checkHas(set, "Card")){
+					valid = false;
+					builder.append(setName + " in " + gameName + " doesn't have any <Card> tags.\n\n");
+				} else {
+					List<Element> cards = set.getChildren("Card");
+					
+					//For each Card in Set
+					for (Element card : cards){
+						//Check that each Card has a Name
+						if(!checkHasName(card)){
+							valid = false;
+							builder.append("A <Card> in " + setName + " in " + gameName + " doesn't have a <Name>.\n\n");
+						} else if (!checkHas(card, "Level")){
+							//If card does have a name, check for Level
+							String cardName = card.getChildText("Name");
+							
+							valid = false;
+							builder.append(cardName + " in " + setName + " in " + gameName + " doesn't have a <Level>.\n\n");
+						}
+					}
 				}
 			}
 		}
@@ -109,146 +168,4 @@ public class XMLChecker {
 	private boolean checkHas(Element e, String s){
 		return e.getChild(s) != null;
 	}
-		/* TODO Remove if new test works.
-		//Checks for a null Document.
-		if(doc == null){
-			return new Validation(false, "Null document.");
-		}
-		
-		//Makes sure Root Element is <PackItBack>
-		if(!checkRootElement()){
-			return new Validation(false, "Root Element incorrect. Should be <PackItBack>");
-		} else {
-			builder.append("XML Checks out.\n");
-		}
-		
-		//Makes sure there is at least 1 <Game> tag.
-		if(!checkGames()){
-			return new Validation(false, "No <Game> tags found!");
-		} else {
-			builder.append(countGames() + "game(s).\n");
-		}
-		
-		//Makes sure all <Game> tags have a <Name> tag.
-		if(!checkGameNames()){
-			return new Validation(false, "Not all <Game> tags have a <Name> tag.");
-		}
-		
-		//Makes sure all <Game> tags have at least one <Set> tag.
-		if(!checkGameSets()){
-			return new Validation(false, "Not all <Game> tags have a <Set> tag.");
-		}
-		
-		//Makes sure all <Set> tags have a <Name> tag.
-		if(!checkSetNames()){
-			return new Validation(false, "Not all <Set> tags have a <Name> tag.");
-		}
-		
-		//Make sure all <Set> tags have at least one <Build> tag.
-		if(!checkSetBuilds()){
-			return new Validation(false, "Not all <Set> tags have at least one <Build> tag.");
-		}
-		
-		//Our "Everything works" return.
-		return new Validation(true, builder.toString());
-	}
-	
-	private boolean checkRootElement(){
-		return doc.getRootElement().getName().equals("PackItBack");
-	}
-	
-	private boolean checkGames(){
-		return countGames() > 0;
-	}
-	
-	private int countGames(){
-
-		int count = 0;
-		
-		for(Element e : doc.getRootElement().getChildren("Game")){
-			count++;
-		}
-		
-		return count;
-	}
-	
-	private boolean checkGameNames(){
-		return getGameNames().size() == countGames();
-	}
-	
-	private ArrayList<String> getGameNames(){
-		
-		ArrayList<String> games = new ArrayList<String>();
-		
-		for(Element e : doc.getRootElement().getChildren("Game")){
-			Element f = e.getChild("Name");
-			
-			if (f != null){
-				games.add(f.getText());
-			}
-		}
-		
-		return games;
-	}
-	
-	private boolean checkGameSets(){
-		
-		//For each Game
-		for (Element e : doc.getRootElement().getChildren("Game")){
-			List<Element> sets = e.getChildren("Set");
-			if(sets.size() >= 1){
-				continue;
-			} else {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	private boolean checkSetNames(){
-		//Get List of Games
-		List<Element> games = doc.getRootElement().getChildren("Game");
-		
-		//For each game
-		for(Element e : games){
-			
-			//Get List of sets
-			List<Element> sets = e.getChildren("Set");
-			
-			//For each set
-			for(Element f : sets){
-				Element n = f.getChild("Name");
-				
-				if(n == null){
-					return false;
-				}
-			}
-		}
-		
-		return true;
-	}
-	
-	private boolean checkSetBuilds(){
-		
-		List<Element> games = doc.getRootElement().getChildren("Game");
-		
-		//For each Game
-		for (Element e : games){
-			List<Element> sets = e.getChildren("Set");
-			
-			//For each Set
-			for (Element f : sets){
-				
-				List<Element> builds = f.getChildren("Build");
-				
-				if(builds.size() == 0){
-					return false;
-				}
-			}
-		}
-		
-		return true;
-	}
-	*/
 }
